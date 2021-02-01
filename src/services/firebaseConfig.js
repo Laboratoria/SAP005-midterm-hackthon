5c0f42e3a10a96bb2bef0820b692e296c2950c2d
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-02-01 15:24:12 -0300

    pagina de menu das listas ok

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCes5POY0arYlDoeS6HqawSH7SFD0cSjcM",
-  authDomain: "midterm-hackthon.firebaseapp.com",
-  projectId: "midterm-hackthon",
-  storageBucket: "midterm-hackthon.appspot.com",
-  messagingSenderId: "418935778179",
-  appId: "1:418935778179:web:7486d8e6441f290e17e4b6"
+  apiKey: 'AIzaSyCes5POY0arYlDoeS6HqawSH7SFD0cSjcM',
+  authDomain: 'midterm-hackthon.firebaseapp.com',
+  projectId: 'midterm-hackthon',
+  storageBucket: 'midterm-hackthon.appspot.com',
+  messagingSenderId: '418935778179',
+  appId: '1:418935778179:web:7486d8e6441f290e17e4b6'
 };
 
 firebase.initializeApp(firebaseConfig);
