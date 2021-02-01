df3cc13d771b97f3014a97eb902feb5be41da71e
Beatriz Penalva <biapenalva@gmail.com>
2021-02-01 19:06:00 -0300

    [html] seção footer

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,19 @@
+import { createFooter } from '../../components/menu/footer/index.js'
+
 export const Main = () => {
-  // Coloque sua página
+
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
+      <section class="main-page">
       <h1>Aqui vai ser a página principal!</h1>
+      <section id="header"></section>
+      <section id="catalogue"></section>
+      <section id="footer"></section>
+    </section>
   `;
+
+  const getFooterSection = rootElement.querySelector('#footer');
+  getFooterSection.appendChild(createFooter());
+
   return rootElement;
 };
