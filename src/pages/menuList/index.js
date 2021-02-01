13599a5eee1bc1156832ebf85cf73ad3de0f5604
Nayara.Fugii <nayarac.neto@gmail.com>
2021-02-01 12:05:21 -0300

    consulta de API printando filmes na tela, estilizado pelo CSS

--- src/pages/menuList/index.js
+++ src/pages/menuList/index.js
@@ -1,8 +1,25 @@
-export const MenuList = () => {
-  const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
-      
-  `;
+let movie = "";
 
-  return rootElement;
+export const allMovies = async () => {
+  let fetchMovies = await fetch(
+    "https://api.themoviedb.org/3/movie/upcoming?api_key=5050539d52522a0616ff555db4437bd6&language=pt-BR"
+  );
+  let list= await fetchMovies.json();
+  let showList = list.results;
+  
+    await showList.forEach(img=> {
+    document.getElementById("root").innerHTML = movie += `
+      <div class = "backgroundPoster">
+        <img class = "poster" src = "https://image.tmdb.org/t/p/original/${img.poster_path}">
+        <div class = "btnAdd">
+          <button>+ASSISTIDO</button>
+          <button>+ASSISTIR</button>
+        </div>
+      </div>
+    </div>`
+  });  
+  
 };
+allMovies();
+
+ 
