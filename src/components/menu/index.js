b3d70e2d5cc5f0b6ae5090f82e51740ba75f7e7f
Beatriz Penalva <biapenalva@gmail.com>
2021-02-01 15:52:18 -0300

    [js] botões funcionando

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -6,25 +6,70 @@ export const createMenu = () => {
   const footerContainer = document.createElement('footer');
   footerContainer.innerHTML = `
     <nav class="menu">
-        <button id="home" class="button-icon-feed"><img src="../../img/logout.png" height="35px" width="35px"></button>
-        <button id="favorites" class="button-icon-feed"><img src="../../img/logout.png" height="35px" width="35px"></button>
-        <button id="watchlist" class="button-icon-feed"><img src="../../img/logout.png" height="35px" width="35px"></button>
-        <button id="discarded" class="button-icon-feed"><img src="../../img/logout.png" height="35px" width="35px"></button>
-        <button id="logout" class="button-icon-feed"><img src="../../img/logout.png" height="35px" width="35px"></button>
+        <button id="home" class="button-icon-feed">
+            <span class="material-icons">home</span>
+        </button>
+
+        <button id="favorites" class="button-icon-feed">
+            <span class="material-icons">favorite</span>
+        </button>
+
+        <button id="watchlist" class="button-icon-feed">
+            <span class="material-icons">bookmark</span>
+        </button>
+        
+        <button id="discarded" class="button-icon-feed">
+            <span class="material-icons">delete</span>
+        </button>
+
+        <button id="logout" class="button-icon-feed">
+            <span class="material-icons">logout</span>
+        </button>
     </nav>
   `;
 
-  //const logOutButton = footerContainer.querySelector('#logout');
-  //logOutButton.addEventListener('click', sendLogOut);
+  const homeButton = footerContainer.querySelector('#home');
+  homeButton.addEventListener('click', sendHome);
+
+  const favoritesButton = footerContainer.querySelector('#favorites');
+  favoritesButton.addEventListener('click', sendFavorites);
+
+  const watchListButton = footerContainer.querySelector('#watchlist');
+  watchListButton.addEventListener('click', sendWatchList);
+
+  const discardedButton = footerContainer.querySelector('#discarded');
+  discardedButton.addEventListener('click', sendDiscarded);
+  
+  const logOutButton = footerContainer.querySelector('#logout');
+  logOutButton.addEventListener('click', sendLogOut);
 
   return footerContainer;
 };
 
-/*
+const sendHome = () => {
+    onNavigate('/');
+};
+
+const sendFavorites = () => {
+  console.log("criar página de favoritos")
+};
+
+const sendWatchList = () => {
+  console.log("criar lista de filmes pra ver")
+};
+
+const sendDiscarded = () => {
+  console.log("criar página de filmes que não quero ver")
+};
+
 const sendLogOut = () => {
-  logOut()
+  console.log("logout")
+/*  logOut()
     .then(() => {
       onNavigate('/');
     });
+*/    
 };
-*/
+
+
+
