65f9b7570a6b2244ad244f3153eff01fe94a803b
Dunia <dunia07@hotmail.com>
2021-02-01 13:56:15 -0300

    feat(structure): estrutura inicial da aplicação contendo main.js, index.html, router.js, separação por páginas e instalação inicial de algumas dependências.

---
+++ src/router.js
@@ -0,0 +1,35 @@
+// Este é seu ponto de entrada da sua aplicação
+import { Home } from './Pages/Home/index.js';
+import { Search } from './Pages/Search/index.js';
+import { onNavigate } from './utils/history.js';
+
+const routeRender = () => {
+  const rootDiv = document.getElementById('root');
+  const routes = {
+    '/' : Home,
+    '/search': Search,
+
+  };
+
+  rootDiv.innerHTML = '';
+  rootDiv.appendChild(routes[window.location.pathname]());
+};
+
+window.addEventListener('popstate', routeRender);
+window.addEventListener('load', () => {
+  document
+    .getElementById('home')
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      onNavigate('/')
+    });
+
+    document
+    .getElementById('search')
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      onNavigate('/search')
+    });
+
+    routeRender();
+  });
---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+export const myFunction = () => {
+    // seu código aqui
+    console.log('Olá mundo!');
+  };
