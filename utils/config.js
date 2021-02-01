486bd359d1d2baf871949d02035e8cb3ca796159
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-02-01 11:13:19 -0300

    iniciado pagina de configurações padrão

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+export default {
+    dataBase: {
+      apiKey: "api_key=9c2c51641b3f3b8d35ac3160b94bf80d",
+      baseURL: "https://api.themoviedb.org/3/",
+      baseImageURL: "https://image.tmdb.org/t/p/",
+      imageSize: "original",
+      language: "&language=pt-br",
+      searchMulti: "search/multi?",
+      watchProviders: "watch/providers?",
+    }
+  };
