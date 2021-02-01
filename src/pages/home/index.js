4316353c85016974e4c6f5a866b1da59e5ac062f
Beatriz Penalva <biapenalva@gmail.com>
2021-02-01 15:09:58 -0300

    [html] organização do rootElement

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,37 +1,57 @@
 import { films } from './mock.js';
+import { createMenu } from '../../components/footerhome.js'
 
 export const Home = () => {
-  
-  const rootElement = document.createElement('div');
-  rootElement.classList.add('container-films')
 
-     function getFilms(i) { 
+  const rootElement = document.createElement('div');
+  rootElement.innerHTML = `
+  <section class="main-page">
+    <section id="header"></section>
+    <section id="catalogue"></section>
+    <section id="menu"></section>
+  </section>
+`;
   
-       fetch(`http://www.omdbapi.com/?t=${i.title}&apikey=ce12da02`)
-          .then(response => response.json())
-          .then(json =>  { console.log(json)
-  
-                 rootElement.innerHTML += ` 
-                 <div class="movie-box">
-                   <img class="image" src="${json.Poster}">
-                    <p>${json.Title}</p>
-                  <div class="details"> 
-                    <p>Awards: ${json.Awards}</p>
-                    <p>Plot: ${json.Plot}</p>
-                    <p>Genre: ${json.Genre}</p>
-                    <p>Director: ${json.Director}</p>
-                    <p>Year: ${json.Year}</p>
-                    <p>IMDb Rating: ${json.imdbRating}</p>
-                    <p>Runtime: ${json.Runtime}</p>
-                  </div>
-                 </div>
-                  `  
-           })     
-     }   
-  
-     for(let i of films){
-      getFilms(i)
-    }
-    
+const getAllFilms = () => {
+  for (let i of films) {
+    getFilms(i);
+  }
+}
+getAllFilms();
+
+const getMenuSection = rootElement.querySelector('#menu');
+  getMenuSection.appendChild(createMenu());
+
   return rootElement;
 };
+
+const getFilms = (i) => {
+  fetch(`http://www.omdbapi.com/?t=${i.title}&apikey=ce12da02`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      const getCatalogueSection = document.querySelector('#catalogue');
+      getCatalogueSection.appendChild(printFilms(json));
+      printFilms(json);
+    })
+}
+
+const printFilms = (json) => {
+  const filmsContainer = document.createElement('section');
+  filmsContainer.innerHTML += `
+    <section class="movie-box">
+        <img class="image" src="${json.Poster}">
+        <p>${json.Title}</p>
+        <section class="details">
+            <p>Awards: ${json.Awards}</p>
+            <p>Plot: ${json.Plot}</p>
+            <p>Genre: ${json.Genre}</p>
+            <p>Director: ${json.Director}</p>
+            <p>Year: ${json.Year}</p>
+            <p>IMDb Rating: ${json.imdbRating}</p>
+            <p>Runtime: ${json.Runtime}</p>
+        </section>
+    </section>
+   `
+   return filmsContainer;
+}
