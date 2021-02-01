bfc4f401ff1a9524785536f22311fad596c14ca0
Nijoane <nicajoane@outlook.com>
2021-02-01 16:40:37 -0300

    [carousel] criando estrutura do carrossel

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,58 +1,60 @@
 /* eslint-disable no-console */
 import { films } from './mock.js';
-import { createMenu } from '../../components/menu/index.js'
+import { createMenu } from '../../components/menu/index.js';
+
+const printFilms = (json) => {
+  const filmsContainer = document.createElement('section');
+  filmsContainer.innerHTML += `
+    <section class="movie-box">
+      <section class="carousel-container">
+        <p>${json.Title}</p>
+        <img class="image" src="${json.Poster}">
+      <section> 
+      <section class="details">
+        <p>Awards: ${json.Awards}</p>
+        <p>Plot: ${json.Plot}</p>
+        <p>Genre: ${json.Genre}</p>
+        <p>Director: ${json.Director}</p>
+        <p>Year: ${json.Year}</p>
+        <p>IMDb Rating: ${json.imdbRating}</p>
+        <p>Runtime: ${json.Runtime}</p>
+      </section>
+    </section> `;
+
+  return filmsContainer;
+};
+
+const getFilms = (i) => {
+  fetch(`http://www.omdbapi.com/?t=${i.title}&apikey=ce12da02`)
+    .then((response) => response.json())
+    .then((json) => {
+      // console.log(json);
+      const getCatalogueSection = document.querySelector('#catalogue');
+      getCatalogueSection.appendChild(printFilms(json));
+
+      printFilms(json);
+    });
+};
 
 export const Home = () => {
-
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
   <section class="main-page">
     <section id="header"></section>
     <section id="catalogue"></section>
     <section id="menu"></section>
-  </section>
-`;
-  
-const getAllFilms = () => {
-  for (const i of films) {
-    getFilms(i);
-  }
-}
-getAllFilms();
+  </section> `;
 
-const getMenuSection = rootElement.querySelector('#menu');
+  const getAllFilms = () => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const i of films) {
+      getFilms(i);
+    }
+  };
+  getAllFilms();
+
+  const getMenuSection = rootElement.querySelector('#menu');
   getMenuSection.appendChild(createMenu());
 
   return rootElement;
 };
-
-const getFilms = (i) => {
-  fetch(`http://www.omdbapi.com/?t=${i.title}&apikey=ce12da02`)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json)
-      const getCatalogueSection = document.querySelector('#catalogue');
-      getCatalogueSection.appendChild(printFilms(json));
-      printFilms(json);
-    })
-}
-
-const printFilms = (json) => {
-  const filmsContainer = document.createElement('section');
-  filmsContainer.innerHTML += `
-    <section class="movie-box">
-        <img class="image" src="${json.Poster}">
-        <p>${json.Title}</p>
-        <section class="details">
-            <p>Awards: ${json.Awards}</p>
-            <p>Plot: ${json.Plot}</p>
-            <p>Genre: ${json.Genre}</p>
-            <p>Director: ${json.Director}</p>
-            <p>Year: ${json.Year}</p>
-            <p>IMDb Rating: ${json.imdbRating}</p>
-            <p>Runtime: ${json.Runtime}</p>
-        </section>
-    </section>
-   `
-   return filmsContainer;
-}
