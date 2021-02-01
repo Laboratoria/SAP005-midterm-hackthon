3ded196760ec95dbe5acfef38d5a1fa341136247
Thais Wemberlaine <thais.wemberlaine@wundermanthompson.com>
2021-02-01 11:53:25 -0300

    Initial config watch page

--- src/pages/watchList/index.js
+++ src/pages/watchList/index.js
@@ -1,8 +1,18 @@
-export const WatchList = () => {
-  const rootElement = document.createElement('div');
-  rootElement.innerHTML = `
-      
-  `;
+import { signOut } from "../../services/index.js";
 
-  return rootElement;
+export const WatchList = () => {
+  const rootElement = document.createElement('header');
+  rootElement.innerHTML = `
+      <nav>
+        <img class="logo-header" src="../images/logo-cinelist.png" alt="Logo CineList"/>
+        <a href="/menuList" id="menu-list">Listas</a>
+        <button type="submit" id="sign-out">SAIR</button>
+      </nav>
+    `;
+  const logoutButton = rootElement.querySelector("#sign-out");
+  logoutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    signOut();
+  })
+ return rootElement;
 };
