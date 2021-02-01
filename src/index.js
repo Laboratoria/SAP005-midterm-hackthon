5ed59293fdb2b770820eff965c94bcef091e84db
letbrasil <le.brasileiro@gmail.com>
2021-02-01 13:54:55 -0300

    doc: api/teste

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import movieGenres from "./genres.js"
 
+let imgBaseUrl = "https://image.tmdb.org/t/p/w200";
+let imgSrc = "";
+
+const getPoster = () => {
+  return fetch(movieGenres.actionNfx)
+    .then(response => response.json())
+    .then(json => json.results[0].poster_path);
+}
+
+getPoster().then(posterPath => {
+  imgSrc = imgBaseUrl + posterPath;
+  document.getElementById("root").innerHTML = `
+  <img src="${imgSrc}">
+  `;
+  console.log(imgSrc);
+})
