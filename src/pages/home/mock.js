62dca242042dcd8788e9eee1446c448939e33fad
Talita-8 <tlthmf1@outlook.com>
2021-02-01 12:05:12 -0300

    movies on screen

---
+++ src/pages/home/mock.js
@@ -0,0 +1,561 @@
+export const films =  [
+
+    // SCI-FI
+
+    {
+      "title" : "Skywatch",
+      "genre" : "sci-fi"
+     },
+    {
+      "title": "Bar talk", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "Sight", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "I'm You, Dickhead", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "Zari", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "Pumzi", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "Blue Season", 
+      "genre": "sci-fi"
+     },
+     {
+      "title": "Alive in Joburg", 
+      "genre": "sci-fi"
+     },
+     {
+      "title" : "The Nostalgist" ,
+      "genre" : "sci-fi"
+     }, 
+     {
+     "title" : "Electronic Labyrinth: THX 1138 4EB", 
+     "genre" : "sci-fi"
+    },
+     {
+      "title" :"Kung Fury",
+      "genre" : "sci-fi"
+     },
+     {
+        "title" :"Ballistic",
+        "genre" : "sci-fi"
+       },
+       {
+        "title" :"The Privates",
+        "genre" : "sci-fi"
+       },
+       {
+         "title" : "La Jetée",
+         "genre" : "sci-fi"
+        },
+
+         // DRAMA
+
+    {
+      "title" : "Wasp",
+      "genre" : "drama"
+    },
+    {
+      "title" : "Two Cars, One Night",
+      "genre" : "drama"
+    },
+    {
+      "title" : "The Good Time Girls",
+      "genre" : "drama"
+    }, 
+    {
+      "title" :"Connection",
+      "genre" : "drama"
+     },
+     {
+      "title" :"The Red Balloon",
+      "genre" : "drama"
+     },
+     {
+      "title" :"The Most Beautiful Thing",
+      "genre" : "drama"
+     },
+     {
+      "title" : "The Phone Call",
+      "genre" : "drama"
+     },
+     {
+      "title" : "Writer's Block",
+      "genre" : "drama"
+     },
+     {
+        "title" : "Session Man",
+        "genre" : "drama"
+     },
+     {
+      "title" : "Gregory Go Boom",
+      "genre" : "drama"
+     },
+     {
+      "title" : "The New West",
+      "genre" : "drama"
+     },
+     {
+        "title" : "Hotel Chevalier",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Small Deaths",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Glory At Sea",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Eight",
+        "genre" : "drama"
+     },
+     {
+      "title" : "Blood is my Name",
+      "genre" : "drama"
+     },
+     {
+        "title" : "Lick The Star",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Carne",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Dead Hearts",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Two Puddles",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Yulia & Juliet",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Pria",
+        "genre" : "drama"
+     },
+     {
+        "title" : "The Lonely Orbit",
+        "genre" : "drama"
+     },
+     {
+      "title" : "Pitch Black Heist",
+      "genre" : "drama"
+   },
+     {
+        "title" : "Out for Delivery",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Um sol Alaranjado",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Menino da calça branca",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Fantasmas",
+        "genre" : "drama"
+     },
+     {
+        "title" : "Olho por olho",
+        "genre" : "drama"
+     },
+     {
+      "title" : "Ritual In Transfigured Time",
+      "genre" : "drama"
+      },
+      {
+         "title" : "The Great Train Robbery",
+         "genre" : "drama"
+      },
+      {
+         "title" : "What Did Jack Do?",
+         "genre" : "drama"
+      },
+      {
+         "title" : "I Feel Stupid",
+         "genre" : "drama"
+      },
+      {
+         "title" : "I am John Wayne",
+         "genre" : "drama"
+      },
+      {
+         "title" : "Multi-Facial",
+         "genre" : "drama"
+      },
+
+
+        // COMEDY
+
+      {
+        "title" : "Validation",
+        "genre" : "comedy"
+      },
+     {
+        "title" : "A Dog’s Life",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "No Time",
+      "genre" : "comedy"
+     },
+     {
+        "title" : "Six Shooter",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "The Last Real Cowboys",
+      "genre" : "comedy"
+     },
+     {
+        "title" : "The Music Box",
+        "genre" : "comedy"
+     },
+     {
+        "title" : "Praça Walt Disney",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "Castello Cavalcanti",
+      "genre" : "comedy"
+   },
+     {
+        "title" : "A Velha a Fiar",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "The New Tenants",
+      "genre" : "comedy"
+   },
+     {
+        "title" : "Eletrodoméstica",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "I'll Wait for the Next One",
+      "genre" : "comedy"
+   },
+     {
+        "title" : "Os Óculos do Vovô",
+        "genre" : "comedy"
+     },
+     {
+      "title" : "Alex's Dream",
+      "genre" : "comedy"
+     },
+     {
+      "title" : "Kleptomami",
+      "genre" : "comedy"
+     },
+     {
+      "title" : "Daytime Noon",
+      "genre" : "comedy"
+     },
+     {
+      "title" : "A Play",
+      "genre" : "comedy"
+     },
+     {
+      "title" : "Paperman",
+      "genre" : "comedy"
+     },
+     
+     // HORROR
+
+     {
+      "title" : "Waffle",
+      "genre" : "horror"
+     },
+     {
+      "title" : "Tuck Me In",
+      "genre" : "horror"
+     },
+     {
+      "title" : "The Jigsaw",
+      "genre" : "horror"
+   },
+     {
+        "title" : "The Big Shave",
+        "genre" : "horror"
+     },
+     {
+        "title" : "The Smiling Man",
+        "genre" : "horror"
+     },
+     {
+        "title" : "The Birch",
+        "genre" : "horror"
+     },
+     {
+        "title" : "Cutting Moments",
+        "genre" : "horror"
+     },
+     {
+        "title" : "Alexia",
+        "genre" : "horror"
+     },
+     {
+        "title" : "Pumpkin Movie",
+        "genre" : "horror"
+     },
+     
+
+     // FANTASY
+
+     {
+      "title" : "Doodlebug",
+      "genre" : "fantasy"
+   },
+     {
+        "title" : "Son of the moon: A Harry Potter fan film",
+        "genre" : "fantasy"
+     },
+     {
+        "title" : "Severus Snape and the Marauders",
+        "genre" : "fantasy"
+     },
+     {
+        "title" : "Life After Fred: Christmas at Hogsmeade",
+        "genre" : "fantasy"
+     },
+     {
+       "title" : "Green Vinyl",
+       "genre" : "fantasy"
+     },
+     {
+       "title" : "Un Chien Andalou",
+       "genre" : "fantasy"
+     },
+     {
+      "title" : "Meshes of the Afternoon",
+      "genre" : "fantasy"
+    },
+
+
+     // ANIMATION
+
+     {
+      "title" : "Alike",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Vincent",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Sitara: Let Girls Dream",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Harvie Krumpet",
+      "genre" : "animation"
+     },
+     {
+        "title" : "Kaiju",
+        "genre" : "animation"
+     },
+     {
+      "title" : "Bao",
+      "genre" : "animation"
+     },
+     {
+        "title" : "Kitbull",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Hair love",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Kaiju Bunraku",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Chez Moi",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Nigel",
+        "genre" : "animation"
+     },
+     {
+        "title" : "One Small Step",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Jonas and The Sea",
+        "genre" : "animation"
+     },
+     {
+        "title" : "23 Degrees 5 Minutes",
+        "genre" : "animation"
+     },
+     {
+        "title" : "A Night in Camp Heebie Jeebie",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Guaxuma",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Torre",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Animando",
+        "genre" : "animation"
+     },
+     {
+        "title" : "Agent 327",
+        "genre" : "animation"
+     },
+     {
+      "title" : "Song Sparrow",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Steamboat Willie",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Luxo Jr.",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Piper",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Street of Crocodiles",
+      "genre" : "animation"
+     },
+     {
+      "title" : "My Moon",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Big Bang Big Boom",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Robots of Brixton",
+      "genre" : "animation"
+     },
+     {
+      "title" : "La Luna",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Balance",
+      "genre" : "animation"
+     },
+     {
+      "title" : "In a Heartbeat",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Logorama",
+      "genre" : "animation"
+     },
+     {
+      "title" : "Purl",
+      "genre" : "animation"
+     },
+     {
+      "title" : "The Alphabet",
+      "genre" : "animation"
+     },
+     {
+      "title" : "The Life of Death",
+      "genre" : "animation"
+     },
+     
+     
+
+     // DOCUMENTARY
+
+     {
+        "title" : "Hearing Madge",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Just a guy",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Stay Close",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Socorro Nobre",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "A pedra da Riqueza",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Aruanda",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Maioria Absoluta",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Arraial do Cabo",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Alma no olho",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "O Poeta do castelo",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Memória do Cangaço",
+        "genre" : "documentary"
+     },
+     {
+        "title" : "Caramujo-flor",
+        "genre" : "documentary"
+     },
+     {
+      "title" : "How Far is Home",
+      "genre" : "documentary"
+     },
+     {
+      "title" : "Guns Found Here",
+      "genre" : "documentary"
+     },
+     {
+      "title" : "Single Room Occupancy",
+      "genre" : "documentary"
+     },
+     {
+      "title" : "The Lady in Number 6: Music Saved My Life",
+      "genre" : "documentary"
+     },
+
+]
