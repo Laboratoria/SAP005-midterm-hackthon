27675853df84781c8086092e4e033d97b8d20392
Nayara.Fugii <nayarac.neto@gmail.com>
2021-02-01 16:59:37 -0300

    função lista

--- src/pages/mainList/index.js
+++ src/pages/mainList/index.js
@@ -2,6 +2,7 @@ let db = firebase.firestore();
 let movie = "";
 const userId = firebase.auth().currentUser.uid
 
+
 export const allMovies = async () => {
   let fetchMovies = await fetch(
     "https://api.themoviedb.org/3/movie/upcoming?api_key=5050539d52522a0616ff555db4437bd6&language=pt-BR"
@@ -10,7 +11,8 @@ export const allMovies = async () => {
   let showList = list.results;
   
     await showList.forEach(img=> {
-    document.getElementById("root").innerHTML = movie += `
+    document.getElementById("root").innerHTML = 
+      movie += `
       <div class = "backgroundPoster id ="${img.poster_path}">
         <img class = "poster"  src = "https://image.tmdb.org/t/p/original/${img.poster_path}">
         <div class = "btnAdd">
