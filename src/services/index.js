099d096fc536cb1896abd95cd069babe62db9af1
Beatriz Penalva <biapenalva@gmail.com>
2021-02-01 12:33:49 -0300

    função de verificar se tem usuário logado

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,1 @@
-// exporte suas funções
-
-export const myFunction = () => {
-  // seu código aqui
-  console.log('Olá mundo!');
-};
+export const verifyUserLogged = (callback) => firebase.auth().onAuthStateChanged(callback);
