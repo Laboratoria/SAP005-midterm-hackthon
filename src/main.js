7c41c1eb2656be433078b43ae56856a277ce13db
Dunia <dunia07@hotmail.com>
2021-02-01 22:01:20 -0300

    feat(search-by-date): layout e input de busca por data

---
+++ src/main.js
@@ -0,0 +1,23 @@
+//parte do script pra chamar o datepicker definido na jQuery UI
+$(function() {
+    $( "#input-date" ).datepicker({
+        dateFormat: 'dd-mm-yy',
+        showOtherMonths: true,
+        dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+        dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+        minDate: 0, 
+        maxDate: "+1M +10D" 
+    });
+});
+
+const date = document.getElementById("input-date")
+const btnDate = document.getElementById("btn-date");
+
+btnDate.addEventListener("click",testeBtn)
+
+function testeBtn(){
+    console.log(date.value)
+}
