251db44bba9b617572c9d6164def4a055221f42f
Beatriz Penalva <biapenalva@gmail.com>
2021-02-02 15:47:31 -0300

    pegando posters e títulos

--- src/pages/watchlist/index.js
+++ src/pages/watchlist/index.js
@@ -1,5 +1,6 @@
 import { header } from '../../components/header/index.js';
 import { createMenu } from '../../components/menu/index.js';
+import { films } from '../home/mock.js'
 
 export const WatchList = () => {
     const rootElement = document.createElement('div');
@@ -17,7 +18,28 @@ export const WatchList = () => {
     const getHeaderSection = rootElement.querySelector('#header');
     getHeaderSection.appendChild(header());
 
-
     return rootElement;
   };
-  
+
+
+  const getFilms = () => {
+    for (const i of films) {
+        fetch(`http://www.omdbapi.com/?t=${i.title}&apikey=ce12da02`)
+            .then((response) => response.json())
+            .then((json) => {
+                const getContainer = document.querySelector('#my-watch-list');
+                getContainer.appendChild(createCards(json));
+                createCards(json)
+            });
+    }        
+  };
+  getFilms();
+
+  function createCards(json) {
+    const cardsContainer = document.createElement('section');
+    cardsContainer.innerHTML += `
+    <p class="title">${json.Title}</p>
+    <img class="image" src="${json.Poster}">
+    `
+    return cardsContainer
+  }
