e80ce2abbef483b12659b15f774b3176904f530f
Beatriz Penalva <biapenalva@gmail.com>
2021-02-02 15:06:12 -0300

    header, menu e estrutura da página

--- src/pages/watchlist/index.js
+++ src/pages/watchlist/index.js
@@ -1,9 +1,23 @@
+import { header } from '../../components/header/index.js';
+import { createMenu } from '../../components/menu/index.js';
+
 export const WatchList = () => {
-    // Coloque sua página
     const rootElement = document.createElement('div');
     rootElement.innerHTML = `
-        <h1>Filmes que eu quero ver!</h1>
+    <section class="lists">
+      <section id="header"></section>
+      <section id="my-watch-list" class="lists"></section>
+      <section id="menu"></section>
+    </section>
     `;
+
+    const getMenuSection = rootElement.querySelector('#menu');
+    getMenuSection.appendChild(createMenu());
+
+    const getHeaderSection = rootElement.querySelector('#header');
+    getHeaderSection.appendChild(header());
+
+
     return rootElement;
   };
   
