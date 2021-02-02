c47de789508c2d77c7e2da3f932759951f075136
Jessica Cavalcante <je-cavalcante@hotmail.com>
2021-02-01 22:19:48 -0300

    criação do component header

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,20 @@
+/*import { header } from '../../components/header/index.js';*/
+
 export const Main = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Aqui vai ser a página principal!</h1>
-  `;
+  <section class="main-page">
+    <section id="header-main"></section>
+    <section id="catalogue-main"></section>
+    <section id="info-main"></section>
+    <section id="menu-main"></section>
+  </section>
+`;
+
+/*const getHeaderSection = rootElement.querySelector('#header-main');
+
+  getHeaderSection.appendChild(header());*/
+
   return rootElement;
 };
